d83af5180d09fed5c40d1b2a11364ce7d346bd7d
Nilesh Sawant <nilsaw101@gmail.com>
2014-05-21 23:48:12 -0400

    Basic changes working further

---
+++ BasicAlgo/src/main/java/com/site/algo/fifolifo/StackData.java
@@ -0,0 +1,8 @@
+package com.site.algo.fifolifo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class StackData {
+
+}
--- BasicAlgo/src/main/java/com/site/algo/main/RunFile.java
+++ BasicAlgo/src/main/java/com/site/algo/main/RunFile.java
@@ -1,5 +1,13 @@
 package com.site.algo.main;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class RunFile {
 
+	public static ApplicationContext context;
+	public static void main(String[] args){
+		
+		context=new ClassPathXmlApplicationContext ("context.xml");
+	}
 }
