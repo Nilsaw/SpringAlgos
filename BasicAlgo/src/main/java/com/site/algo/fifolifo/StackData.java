30bbd2b897855b995c9ac2353f77d2a9074d948b
Nilesh Sawant <nilsaw101@gmail.com>
2014-05-23 23:47:56 -0400

    InPlace Stack reversal.

--- BasicAlgo/src/main/java/com/site/algo/fifolifo/StackData.java
+++ BasicAlgo/src/main/java/com/site/algo/fifolifo/StackData.java
@@ -3,17 +3,18 @@ package com.site.algo.fifolifo;
 import java.util.*;
 
 import org.springframework.stereotype.Component;
+
 /* 
  * Stack time complexity :- Push:- O(1)
  * 				 			Pop :- O(1)
  * 			 	 			Peek:- O(1)
  * 							search :- O(1) - Best / O(n) - Avg
-*/
+ */
 
 @Component
 public class StackData {
 	private Stack<String> stack;
-	
+
 	public Stack<String> getStack() {
 		return stack;
 	}
@@ -21,18 +22,39 @@ public class StackData {
 	public void setStack(Stack<String> stack) {
 		this.stack = stack;
 	}
-	
-	/*Stack operations*/
 
-	public void putData(String data){
+	/* Stack operations */
+
+	public void putData(String data) {
 		stack.push(data);
 	}
-	
-	public String getData(){
+
+	public String getData() {
 		return stack.pop();
 	}
-	
-	public String peekData(){
+
+	public String peekData() {
 		return stack.peek();
+	}
+
+	/**
+	 * call this function to reverse inplace stack
+	 */
+	void reverseStack() {
+		if (!stack.isEmpty()) {
+			String data = getData();
+			reverseStack();
+			putData(data);
+		}
+	}
+
+	void pushInStack(String a) {
+		if (stack.isEmpty()) {
+			putData(a);;
+			return;
+		}
+		String b = stack.pop();
+		pushInStack(a);
+		putData(b);
 	}
 }
